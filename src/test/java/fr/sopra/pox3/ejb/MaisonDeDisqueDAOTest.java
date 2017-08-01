343236c6fe8521655e716b98fbcf82dc4119801f
PH <ph.cazorla@gmail.com>
2017-08-01 13:38:11 +0200

    update pom.xml form Arnaud + test creation

---
+++ src/test/java/fr/sopra/pox3/ejb/MaisonDeDisqueDAOTest.java
@@ -0,0 +1,34 @@
+package fr.sopra.pox3.ejb;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MaisonDeDisqueDAOTest {
+
+	@Test
+	public void testFindById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteById() {
+		fail("Not yet implemented");
+	}
+
+}
